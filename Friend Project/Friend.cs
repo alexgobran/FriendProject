6ee71fb10603c469a8b66a3ba9e05a89552d18d8
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-29 13:47:07 -0400

    Add project files.

---
+++ Friend Project/Friend.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Friend_Project {
+    class Friend {
+    }
+}
---
+++ Friend Project/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Friend_Project {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
