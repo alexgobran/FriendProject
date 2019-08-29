1619b9259cbfb222602c67bcc88c38977d4ed91e
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-29 16:01:05 -0400

    used dictionary, list, array

--- Friend Project/Friend.cs
+++ Friend Project/Friend.cs
@@ -2,7 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Friend_Project {
     class Friend {
+
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool BFF { get; set; }
+        public int HowLong { get; set; }
+
+        public Friend(string name, string phone, string email, bool bff, int howlong) {
+            this.Name = name;
+            this.Phone = phone;
+            this.Email = email;
+            this.BFF = bff;
+            this.HowLong = howlong;
+
+        }
+
+        public Friend() {
+            int i = 0;
+            i.ToString();
+            bool b = true;
+            b.ToString();
+
+        }
+        public override string ToString() {
+            return $"{Name, -15} {Phone, -12} {Email, -25} {(BFF ? "BFF" : ""),3} {HowLong }";
+
+            }
+        }
     }
-}
+
--- Friend Project/Program.cs
+++ Friend Project/Program.cs
@@ -1,9 +1,48 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Friend_Project {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            //  creating a List      (variable name)
+            List<int> integers = new List<int>();
+            integers.Add(17);
+            integers.Add(0);
+            integers.Add(88);
+            integers.Add(43);
+            var count = integers.Count;  //how many items in cllection
+            integers.Sort();
+            integers.Remove(0);
+
+            var f1 = new Friend("Bob", "513-322-4544", "bi@gmail.com", true, 1);
+            var f2 = new Friend("Bill", "513-322-4544", "bi@gmail.com", true, 1);
+            var f3 = new Friend("Bo", "513-322-4544", "bi@gmail.com", false, 1);
+            var f4 = new Friend("Bobby", "513-322-4544", "bi@gmail.com", true, 1);
+            var f5 = new Friend("Bobbi", "513-322-4544", "bi@gmail.com", false, 1);
+
+            //var friends = new Friend[] { f1, f2, f3, f4, f5 };
+             var friends = new List<Friend>();
+
+             friends.Add(f1);
+             friends.Add(f2);
+             friends.Add(f3);
+             friends.Add(f4);
+             friends.Add(f5);
+
+            var aFriend = friends[0];
+
+            foreach (var friend in friends)
+            {
+                 friend.HowLong = (friend.BFF? 10 : 0);
+                Console.WriteLine(friend);
+            }
+
+            //Create a dictionary
+            var StateCodes = new Dictionary<string, string>();
+            StateCodes.Add("OH", "Ohio");
+            StateCodes.Add("KY", "Kentucky");
+            StateCodes.Add("IN", "Indiana");
+            var ohio = StateCodes{ "OH"};
         }
+        
     }
-}
+} 
